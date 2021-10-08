906ad01903ea97ececabd8629be25045ebc68eab
roctbb <roctbb@gmail.com>
2021-10-08 13:22:16 +0300

    initial

--- manage.py
+++ manage.py
@@ -2,9 +2,19 @@ from flask import Flask
 from models import db
 from flask_script import Manager
 from flask_migrate import Migrate
-
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 from config import *
 
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[FlaskIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
 
 app = Flask(__name__)
 db_string = "postgresql://{}:{}@{}:{}/{}".format(DB_LOGIN, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
