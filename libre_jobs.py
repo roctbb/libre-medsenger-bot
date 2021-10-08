8f0fb157c96e20f7077fa48356d4f4b84286296f
roctbb <roctbb@gmail.com>
2021-10-08 16:10:00 +0300

    initial

--- libre_jobs.py
+++ libre_jobs.py
@@ -2,6 +2,6 @@ from libre_bot import *
 from apscheduler.schedulers.background import BlockingScheduler
 
 scheduler = BlockingScheduler()
-scheduler.add_job(sender, 'interval', days=1, args=(app, ))
+scheduler.add_job(sender, 'cron', hour=10, minute=0, second=0, args=(app, ))
 
 scheduler.start()
