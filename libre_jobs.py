887cfd6cdf937f10fbfc03274b07a29df2f488c4
roctbb <roctbb@gmail.com>
2021-10-14 10:42:23 +0300

    move to rq

--- libre_jobs.py
+++ libre_jobs.py
@@ -1,7 +1,12 @@
 from libre_bot import *
 from apscheduler.schedulers.background import BlockingScheduler
+import logging
+
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 scheduler = BlockingScheduler()
-scheduler.add_job(sender, 'cron', hour=10, minute=37, second=0, args=(app, ))
+scheduler.add_job(lambda:print("ping"), 'interval', hours=1)
+scheduler.add_job(sender, 'cron', hour=10, minute=0, second=0, args=(app, ))
 
 scheduler.start()
