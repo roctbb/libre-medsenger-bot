05847d77b17e337562eaf4838da935576b5c1512
roctbb <roctbb@gmail.com>
2021-12-21 17:15:32 +0300

    move to rq

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ from libre_api import *
 from libre_bot import Contract
 
 def test_create_client():
-    client = create_client(True)
+    client = create_client()
 
 def test_get_reports():
     contracts = [Contract(name="Бородин Ростислав Алексеевич", birthday="27/4/1994")]
@@ -10,3 +10,6 @@ def test_get_reports():
 
 def test_prepare_last_file():
     prepare_last_file()
+
+if __name__ == "__main__":
+    test_create_client()
