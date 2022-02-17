b5f9ad0a936668a11f9775356618962e5d6a2c4f
roctbb <roctbb@gmail.com>
2022-02-17 10:23:51 +0300

    text jobs

--- libre_jobs.py
+++ libre_jobs.py
@@ -7,6 +7,6 @@ logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 scheduler = BlockingScheduler()
 scheduler.add_job(lambda:print("ping"), 'interval', hours=1)
-scheduler.add_job(sender, 'cron', hour=10, minute=0, second=0, args=(app, ))
+scheduler.add_job(sender, 'cron', hour=10, minute=24, second=0, args=(app, ))
 
 scheduler.start()
